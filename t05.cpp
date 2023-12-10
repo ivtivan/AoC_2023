5d79dd48577bb2ddd41c69c9950984c4bb749663
Ivan <48709677+ivtivan@users.noreply.github.com>
2023-12-10 10:32:27 +0100

    structured just slightly better

--- t05.cpp
+++ t05.cpp
@@ -8,6 +8,8 @@
 #include <utility>
 #include <vector>
 
+using translation = std::tuple<int64_t, int64_t, int64_t>;
+
 auto split(const std::string& data, char sep = ' ') -> std::pair<std::string, std::string> {
     auto sep_idx = data.find(sep);
     return {data.substr(0, sep_idx), data.substr(sep_idx + 1)};
@@ -29,18 +31,18 @@ auto to_int64_all(const std::vector<std::string>& data) -> std::vector<int64_t> 
     return ints;
 }
 
-auto to_int64_three(const std::vector<std::string>& data) -> std::tuple<int64_t, int64_t, int64_t> {
+auto to_int64_three(const std::vector<std::string>& data) -> translation {
     return {std::stoll(data.at(0)), std::stoll(data.at(1)), std::stoll(data.at(2))};
 }
 
-auto translate_seeds(const std::vector<int64_t>& seeds, const std::vector<std::vector<std::tuple<int64_t, int64_t, int64_t>>>& translations)
+auto translate_seeds(const std::vector<int64_t>& seeds, const std::vector<std::vector<translation>>& translations)
     -> std::vector<int64_t> {
 
     std::vector<int64_t> curr = seeds;
     for (const auto& translation_table : translations) {
         for (int i = curr.size() - 1; i >= 0; --i) {
-            auto lower = std::lower_bound(translation_table.begin(), translation_table.end(), std::tuple<int64_t, int64_t, int64_t>{0, curr[i], 0},
-                [&](const std::tuple<int64_t, int64_t, int64_t>& a, const std::tuple<int64_t, int64_t, int64_t>& b) {
+            auto lower = std::lower_bound(translation_table.begin(), translation_table.end(), translation{0, curr[i], 0},
+                [&](const translation& a, const translation& b) {
                     return get<1>(a) <= get<1>(b);
             });
             if (lower == translation_table.begin()) continue;
@@ -54,72 +56,58 @@ auto translate_seeds(const std::vector<int64_t>& seeds, const std::vector<std::v
     return curr;
 }
 
+auto get_tranlation_tables(std::ifstream& input) -> std::vector<std::vector<translation>> {
+    std::vector<std::vector<translation>> translation_tables;
+    std::vector<translation> curr_table;
+    
+    std::string row;
+    while (std::getline(input, row)) {
+        if (std::find(row.cbegin(), row.cend(), ':') != row.end() || row.empty()) {
+            if(!curr_table.empty()) {
+                std::sort(curr_table.begin(), curr_table.end(), [](translation a, translation b) {
+                    return get<1>(a) < get<1>(b);
+                });
+                translation_tables.push_back(std::move(curr_table));
+                curr_table.clear();
+            }
+            continue;
+        }
+        curr_table.push_back(to_int64_three(get_tokens(row)));
+    }
+    std::sort(curr_table.begin(), curr_table.end(), [](translation a, translation b) {
+        return get<1>(a) < get<1>(b);
+    });
+    translation_tables.push_back(std::move(curr_table));
+
+    return translation_tables;
+}
 
 auto part_1(std::ifstream& input) -> uint64_t {
     std::string raw_seeds;
-    std::string row;
-    std::vector<std::vector<std::tuple<int64_t, int64_t, int64_t>>> translations;
-    std::vector<std::tuple<int64_t, int64_t, int64_t>> curr_translation;
 
     std::getline(input, raw_seeds);
     auto seeds = to_int64_all(get_tokens(split(raw_seeds, ':').second));
 
-    // delete empty line from input
-    while (std::getline(input, row)) {
-        if (std::find(row.cbegin(), row.cend(), ':') != row.end() || row.empty()) {
-            if(!curr_translation.empty()) {
-                std::sort(curr_translation.begin(), curr_translation.end(), [](std::tuple<int64_t, int64_t, int64_t> a, std::tuple<int64_t, int64_t, int64_t> b) {
-                    return get<1>(a) < get<1>(b);
-                });
-                translations.push_back(std::move(curr_translation));
-                curr_translation.clear();
-            }
-            continue;
-        }
-        curr_translation.push_back(to_int64_three(get_tokens(row)));
-    }
-    std::sort(curr_translation.begin(), curr_translation.end(), [](std::tuple<int64_t, int64_t, int64_t> a, std::tuple<int64_t, int64_t, int64_t> b) {
-        return get<1>(a) < get<1>(b);
-    });
-    translations.push_back(std::move(curr_translation));
+    auto translation_tables = get_tranlation_tables(input);
 
-    auto results = translate_seeds(seeds, translations);
+    auto results = translate_seeds(seeds, translation_tables);
     return *std::min_element(results.begin(), results.end());
 }
 
 auto part_2(std::ifstream& input) -> uint64_t {
     std::string raw_seeds;
-    std::string row;
-    std::vector<std::vector<std::tuple<int64_t, int64_t, int64_t>>> translations;
-    std::vector<std::tuple<int64_t, int64_t, int64_t>> curr_translation;
 
     std::getline(input, raw_seeds);
     auto seeds = to_int64_all(get_tokens(split(raw_seeds, ':').second));
 
-    // delete empty line from input
-    while (std::getline(input, row)) {
-        if (std::find(row.cbegin(), row.cend(), ':') != row.end() || row.empty()) {
-            if(!curr_translation.empty()) {
-                std::sort(curr_translation.begin(), curr_translation.end(), [](std::tuple<int64_t, int64_t, int64_t> a, std::tuple<int64_t, int64_t, int64_t> b) {
-                    return get<1>(a) < get<1>(b);
-                });
-                translations.push_back(std::move(curr_translation));
-                curr_translation.clear();
-            }
-            continue;
-        }
-        curr_translation.push_back(to_int64_three(get_tokens(row)));
-    }
-    std::sort(curr_translation.begin(), curr_translation.end(), [](std::tuple<int64_t, int64_t, int64_t> a, std::tuple<int64_t, int64_t, int64_t> b) {
-        return get<1>(a) < get<1>(b);
-    });
-    translations.push_back(std::move(curr_translation));
+    auto translation_tables = get_tranlation_tables(input);
+
 
     std::vector<int64_t> min_from_group;
     for (uint64_t i = 0; i < seeds.size(); i += 2) {
         std::vector<int64_t> group(seeds[i + 1]);
         std::iota(group.begin(), group.end(), seeds[i]);
-        auto results = translate_seeds(group, translations);
+        auto results = translate_seeds(group, translation_tables);
         min_from_group.push_back(*std::min_element(results.begin(), results.end()));
         std::cout << min_from_group.back() << std::endl;
     }
