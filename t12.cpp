34687a99a78e82c47aceafbf79de5f35d7775c7b
Ivan <48709677+ivtivan@users.noreply.github.com>
2023-12-12 22:30:51 +0100

    I hope I remeber to fix it

--- t12.cpp
+++ t12.cpp
@@ -6,111 +6,146 @@
 #include <string>
 #include <vector>
 
-auto tokenize(const std::string& data) -> std::vector<short> {
-    std::vector<short> tokens;
+auto tokenize(const std::string& data) -> std::vector<uint64_t> {
+    std::vector<uint64_t> tokens;
     auto pos {data.find(',')};
     auto prev{0};
     while (pos != std::string::npos) {
-        tokens.push_back(static_cast<short>(std::stoi(data.substr(prev, pos - prev))));
+        tokens.push_back(std::stoll(data.substr(prev, pos - prev)));
         prev = pos + 1;
         pos = data.find(',', prev);
     }
-    tokens.push_back(static_cast<short>(std::stoi(data.substr(prev))));
+    tokens.push_back(std::stoll(data.substr(prev)));
 
     return tokens;
 }
 
-//  returns a bit mask
-auto get_unknown(const std::string& springs) -> uint32_t {
-    uint32_t unknown{0};
-    for (auto& spring : springs) {
-        unknown = (unknown << 1);
-        if (spring == '?') {
-            ++unknown;
+struct Solver {
+private:
+    std::vector<uint64_t> res;
+public:
+    std::string springs;
+    std::vector<uint64_t> spring_data;
+    std::vector<uint64_t> look_up;
+    uint64_t springs_size;
+    uint64_t spring_data_size;
+
+    void setup() {
+        spring_data_size = spring_data.size();
+        springs += '.';
+        springs_size = springs.size();
+
+        look_up.reserve(springs_size - 1);
+        for (uint64_t i = 0; i < springs_size - 1; ++i) {
+            look_up[i] = springs.find('.', i + 1);
+        }
+
+        res.assign(spring_data_size * springs_size, ULLONG_MAX);
+    }
+
+    inline uint64_t get_from_res(uint64_t s_idx, uint64_t d_idx) const {
+        return res[s_idx * spring_data_size + d_idx];
+    }
+
+    inline void set_in_res(uint64_t s_idx, uint64_t d_idx, uint64_t val) {
+        res[s_idx * spring_data_size + d_idx] = val;
+    }
+
+    void print () {
+        for(uint64_t i = 0; i < spring_data_size; ++i) {
+            for(uint64_t j = 0; j < springs_size; ++j) {
+                std::cout << get_from_res(i, j) << " ";
+            }
+            std::cout << std::endl;
         }
     }
 
-    return unknown;
-}
-
-auto get_consecutive(const std::string& springs) -> std::vector<short> {
-    std::vector<short> consecutive;
-    bool counting = false;
-    short curr{0};
-    for (const auto el : springs) {
-        if (el == '#') {
-            if (!counting) counting = true;
-            ++curr;
+    auto find_possible(uint64_t s_idx, uint64_t d_idx) -> uint64_t {
+        if(s_idx >= springs_size) {
+            return d_idx == spring_data_size;
         }
-        else if (counting) {
-                consecutive.push_back(curr);
-                curr = 0;
-                counting = false;
-        }        
-    }
-    if (counting) consecutive.push_back(curr);
+        if (d_idx == spring_data_size) {
+            return springs.find('#', s_idx) == std::string::npos;
+        }
+        if (!(get_from_res(s_idx, d_idx) == ULLONG_MAX)) {
+            return get_from_res(s_idx, d_idx);
+        }
+        switch(springs[s_idx]) {
+            case '#': {
+                auto num_cons = spring_data[d_idx];
+                if (s_idx + num_cons <= look_up[s_idx] && springs[s_idx + num_cons] != '#') {
+                    uint64_t tmp = find_possible(s_idx + num_cons + 1, d_idx + 1);
+                    // std::cout << "1 loads " << tmp << std::endl;
+                    set_in_res(s_idx, d_idx, tmp);
+                    return tmp;
+                }
+                set_in_res(s_idx, d_idx, 0);
+                return 0;
+            }
+            case '.':{
+                uint64_t tmp = find_possible(s_idx + 1, d_idx);
+                set_in_res(s_idx, d_idx, tmp);
+                return tmp;
+            }
+            default:{
+                uint64_t tmp {0};
+                // #
+                auto num_cons = spring_data[d_idx];
+                if (look_up[s_idx] >= s_idx + num_cons && springs[s_idx + num_cons] != '#') {
+                    tmp += find_possible(s_idx + num_cons + 1, d_idx + 1);
+                }
 
-    return consecutive;
-}
-
-auto place_springs(std::string springs, uint32_t setup) -> std::string {
-    for (short i = 0; i < 32 ; ++i) {
-        uint32_t curr = (1 << i);
-        if (curr > setup) break;
-        if ((curr & setup)) {
-            springs[springs.size() - 1 - i] = '#';
-
+                tmp += find_possible(s_idx + 1, d_idx);
+                set_in_res(s_idx, d_idx, tmp);
+                return tmp;
+            }
         }
     }
-    return springs;
-}
-
-auto find_possible(std::string springs, const std::vector<short>& spring_data) -> uint64_t {
-    uint64_t possible {0};
-    const auto unknown_springs {get_unknown(springs)};
-    std::replace(springs.begin(), springs.end(), '?', '.');
-
-    uint32_t curr{0};
-    std::vector<short> parsed;
-    std::vector<uint32_t> tried;
-    for (uint32_t i = 0; i <= unknown_springs; ++i) {
-        curr = (i & unknown_springs);
-        if (std::find(tried.begin(), tried.end(), curr) != tried.end()) continue;
-
-        auto possibility = place_springs(springs, curr);
-        parsed = get_consecutive(possibility);  
-        if (parsed == spring_data) {
-            ++possible;
-        }
-        tried.push_back(curr);
-    }
-    return possible;
-}
+};
 
 auto part_1(std::ifstream& input) -> uint64_t {
     std::string line;
     uint64_t num_possible{0};
-    uint32_t cnt {0};
     while (std::getline(input, line)) {
         auto boundry = line.find(' ');
-
-        auto springs = line.substr(0, boundry);
-        auto spring_data = tokenize(line.substr(boundry + 1)); 
-
-        num_possible += find_possible(springs, spring_data);
-        std::cout << ++cnt << std::endl;
+        Solver s;
+        s.springs = line.substr(0, boundry);
+        s.spring_data = tokenize(line.substr(boundry + 1)); 
+        s.setup();
+        num_possible += s.find_possible(0, 0);
     }
 
     return num_possible;
 }
 
 auto part_2(std::ifstream& input) -> uint64_t {
-    return 0;
-}
+    std::string line;
+    uint64_t num_possible{0};
+    uint64_t cnt{0};
+    while (std::getline(input, line)) {
+        auto boundry = line.find(' ');
+        Solver s;
+        s.springs = line.substr(0, boundry);
+        s.spring_data = tokenize(line.substr(boundry + 1)); 
+        std::vector<uint64_t> spring_data_combined;
+        s.springs += "?" + s.springs + "?" + s.springs + "?" + s.springs + "?" + s.springs;
+        uint16_t spins{5};
+        for (int i = 0; i < spins; ++i) {
+            spring_data_combined.insert(spring_data_combined.end(), s.spring_data.begin(), s.spring_data.end());
+        }
+        s.spring_data = spring_data_combined;
+        
+        std::cout << std::endl;
+        std::cout << ++cnt << std::endl;
+        s.setup();
+        num_possible += s.find_possible(0, 0);
+    }
+
+    return num_possible;}
 
 
 int main() {
     std::ifstream input("inputs/AoC_12.txt");
-    std::cout << "Part 1: " << part_1(input) << std::endl;
-    // std::cout << "Part 2: " << part_2(input) << std::endl;
+    // std::cout << "Part 1: " << part_1(input) << std::endl;
+    std::cout << "Part 2: " << part_2(input) << std::endl;
 }
